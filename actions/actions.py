c08983d5af44cd9049e1971a1361536c0204f6bb
Oleksandr Tereshchenko <toporok@gmail.com>
2021-05-10 20:50:43 +0300

    in progress: action_create_room

--- actions/actions.py
+++ actions/actions.py
@@ -1,10 +1,16 @@
+import logging
 import os
+from pprint import pformat
 from typing import Any, Text, Dict, List
 
+import aiohttp
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-DAILY_CO_TOKEN = os.environ['DAILY_CO_TOKEN']
+logger = logging.getLogger(__name__)
+
+DAILY_CO_BASE_URL = 'https://api.daily.co/v1'
+DAILY_CO_API_TOKEN = os.environ['DAILY_CO_API_TOKEN']
 
 
 class ActionCreateRoom(Action):
@@ -17,7 +23,32 @@ class ActionCreateRoom(Action):
             tracker: Tracker,
             domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-        # dispatcher.utter_message(text=str(tracker.sender_id))
-        dispatcher.utter_template('utter_video_link', tracker, room_link='https://roomlink')
+        async with aiohttp.ClientSession() as session:
+            room_data = {
+                'privacy': 'public',
+                'properties': {
+                    'enable_network_ui': False,
+                    'enable_prejoin_ui': True,
+                    'enable_new_call_ui': True,
+                    'enable_screenshare': True,
+                    'enable_chat': True,
+                    'start_video_off': False,
+                    'start_audio_off': False,
+                    'owner_only_broadcast': False,
+                    'lang': 'en',
+                },
+            }
+            async with session.post(
+                    f"{DAILY_CO_BASE_URL}/rooms",
+                    headers={
+                        'Authorization': f"Bearer {DAILY_CO_API_TOKEN}",
+                    },
+                    json=room_data,
+            ) as resp:
+                created_room = await resp.json()
+                if logger.isEnabledFor(logging.DEBUG):
+                    logger.debug('NEW DAILY CO ROOM:\n%s', pformat(created_room))
+
+        dispatcher.utter_template('utter_video_link', tracker, room_link=created_room['url'])
 
         return []
