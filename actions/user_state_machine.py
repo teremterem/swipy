1e785d323f5a4dedee349bda129ad343dd4d715a
Oleksandr Tereshchenko <toporok@gmail.com>
2021-05-12 15:23:13 +0300

    in progress: UserStateMachine

---
+++ actions/user_state_machine.py
@@ -0,0 +1,41 @@
+class UserState:
+    NEW = 'new'
+    WANTS_CHITCHAT = 'wants_chitchat'
+    OK_FOR_CHITCHAT = 'ok_for_chitchat'
+    DO_NOT_DISTURB = 'do_not_disturb'
+
+
+class UserSubState:
+    ASKED_TO_JOIN = 'asked_to_join'
+    WAITING_FOR_RESPONSE_TO_JOIN = 'waiting_for_response_to_join'
+    ASKED_FOR_READINESS = 'asked_for_readiness'
+    WAITING_FOR_READINESS_RESPONSE = 'waiting_for_readiness_response'
+    CHITCHAT_IN_PROGRESS = 'chitchat_in_progress'
+    MAYBE_DO_NOT_DISTURB = 'maybe_do_not_disturb'
+
+
+class UserStateMachine:
+    def __init__(self, id):
+        self.id = id
+        self.state = UserState.NEW
+        self.sub_state = None
+        self.sub_state_with_whom = None
+        self.sub_state_expiration = None
+
+
+class _UserVault:
+    def __init__(self):
+        self._users = {}
+
+    def get_user(self, user_id):
+        user_state_machine = self._users.get(user_id)
+
+        if user_state_machine is None:
+            user_state_machine = UserStateMachine(user_id)
+            self._users[user_id] = user_state_machine
+
+        return user_state_machine
+
+
+UserVault = _UserVault
+user_vault = UserVault()
---
+++ tests/test_user_state_machine.py
@@ -0,0 +1,29 @@
+import pytest
+
+from actions.user_state_machine import UserVault, UserStateMachine
+
+
+@pytest.fixture
+def user_vault():
+    return UserVault()
+
+
+def test_get_new_user(user_vault):
+    assert user_vault._users == {}
+    user_state_machine = user_vault.get_user('new_user_id')
+
+    assert user_state_machine.id == 'new_user_id'
+    assert user_state_machine.state == 'new'
+    assert user_state_machine.sub_state is None
+    assert user_state_machine.sub_state_with_whom is None
+    assert user_state_machine.sub_state_expiration is None
+
+    assert user_vault._users['new_user_id'] is user_state_machine
+
+
+def test_get_existing_user(user_vault):
+    assert user_vault._users == {}
+    user_state_machine = UserStateMachine('existing_user_id')
+    user_vault._users['existing_user_id'] = user_state_machine
+
+    assert user_vault.get_user('existing_user_id') is user_state_machine
