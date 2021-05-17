a9aae88968c24e3d6a840d893da8d865c175b155
Oleksandr Tereshchenko <toporok@gmail.com>
2021-05-17 18:59:18 +0300

    in progress: employing pytransitions state machine

--- actions/user_state_machine.py
+++ actions/user_state_machine.py
@@ -1,15 +1,31 @@
 from dataclasses import dataclass
 from typing import Text
 
-
-class UserState:
-    NEW = 'new'
-    WANTS_CHITCHAT = 'wants_chitchat'
-    OK_FOR_CHITCHAT = 'ok_for_chitchat'
-    DO_NOT_DISTURB = 'do_not_disturb'
+from transitions import Machine
 
 
 @dataclass
-class UserStateMachine:
+class UserModel:
     user_id: Text
-    state: Text = UserState.NEW
+    state: Text = None
+
+
+class UserStateMachine(UserModel):
+    STATE_NEW = 'new'
+    STATE_WANTS_CHITCHAT = 'wants_chitchat'
+    STATE_OK_FOR_CHITCHAT = 'ok_for_chitchat'
+    STATE_DO_NOT_DISTURB = 'do_not_disturb'
+
+    STATES = [
+        STATE_NEW,
+        STATE_WANTS_CHITCHAT,
+        STATE_OK_FOR_CHITCHAT,
+        STATE_DO_NOT_DISTURB,
+    ]
+
+    def __init__(self, *args, state=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.machine = Machine(model=self, states=self.STATES, initial=self.STATE_NEW)
+        if state is not None:
+            self.machine.set_state(state)
--- tests/actions/data/user_vault_fixtures.py
+++ tests/actions/data/user_vault_fixtures.py
@@ -3,12 +3,13 @@ from typing import List, Dict, Text, Any
 
 import pytest
 
-from actions.user_state_machine import UserStateMachine, UserState
+from actions.user_state_machine import UserStateMachine
 
 
 def _populate_user(user_state_machine: UserStateMachine) -> UserStateMachine:
     from actions.aws_resources import user_state_machine_table
 
+    # noinspection PyDataclass
     user_state_machine_table.put_item(Item=asdict(user_state_machine))
     return user_state_machine
 
@@ -22,7 +23,7 @@ def unit_test_user(create_user_state_machine_table) -> UserStateMachine:
 def user1(create_user_state_machine_table) -> UserStateMachine:
     return _populate_user(UserStateMachine(
         user_id='existing_user_id1',
-        state=UserState.OK_FOR_CHITCHAT,
+        state=UserStateMachine.STATE_OK_FOR_CHITCHAT,
     ))
 
 
