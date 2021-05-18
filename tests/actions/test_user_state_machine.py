0a11d7b9ad6ab139536980412df26cb5047d2ba6
Oleksandr Tereshchenko <toporok@gmail.com>
2021-05-18 12:53:37 +0300

    user states

--- tests/actions/test_user_state_machine.py
+++ tests/actions/test_user_state_machine.py
@@ -2,17 +2,23 @@ from typing import Text
 
 import pytest
 
-from actions.user_state_machine import UserStateMachine
+from actions.user_state_machine import UserStateMachine, UserState
 
-
-@pytest.mark.parametrize('source_state', [
+all_expected_states = [
     'new',
     'waiting_partner_answer',
     'ok_for_chitchat_newbie',
     'ok_for_chitchat_veteran',
     'asked_to_join',
     'do_not_disturb',
-])
+]
+
+
+def test_all_expected_states() -> None:
+    assert UserState.all == all_expected_states
+
+
+@pytest.mark.parametrize('source_state', all_expected_states)
 def test_request_chitchat(source_state: Text) -> None:
     user = UserStateMachine(
         user_id='some_user_id',
