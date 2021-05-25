aea812e22247c5756058554b4d53844d93c88f75
Oleksandr Tereshchenko <toporok@gmail.com>
2021-05-25 12:29:17 +0300

    unit tests for "make ActionDoNotDisturb force the original sender of the declined invitation to find another partner"

--- tests/actions/test_rasa_callbacks.py
+++ tests/actions/test_rasa_callbacks.py
@@ -58,3 +58,27 @@ async def test_join_room(
 
     await rasa_callbacks.join_room('a_receiving_user', 'a_sending_user', 'https://room-unittest/url')
     rasa_core_callback_mock.assert_called_once()
+
+
+@pytest.mark.asyncio
+async def test_find_partner(
+        mock_aioresponses: aioresponses,
+        external_intent_response: Dict[Text, Any],
+) -> None:
+    def rasa_core_callback(url, json=None, **kwargs):
+        assert json == {
+            'name': 'EXTERNAL_find_partner',
+            'entities': {},
+        }
+        return CallbackResult(payload=external_intent_response)
+
+    rasa_core_callback_mock = MagicMock(side_effect=rasa_core_callback)
+    # noinspection HttpUrlsUsage
+    mock_aioresponses.post(
+        'http://rasa-unittest:5005/unittest-core/conversations/a_receiving_user/trigger_intent'
+        '?output_channel=telegram&token=rasaunittesttoken',
+        callback=rasa_core_callback_mock,
+    )
+
+    await rasa_callbacks.find_partner('a_receiving_user')
+    rasa_core_callback_mock.assert_called_once()
