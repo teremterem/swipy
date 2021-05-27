0de77315b653342368a887ffbf992282a0c9d963
Oleksandr Tereshchenko <toporok@gmail.com>
2021-05-27 19:47:29 +0300

    in progress: test_actions - mock things on on a deeper abstraction level for tests to be more effective

--- tests/actions/test_rasa_callbacks.py
+++ tests/actions/test_rasa_callbacks.py
@@ -1,26 +1,33 @@
-from typing import Dict, Text, Any, Callable
-from unittest.mock import AsyncMock
+from typing import Dict, Text, Any, Callable, Tuple
+from unittest.mock import AsyncMock, call
 
 import pytest
+from aioresponses import CallbackResult, aioresponses
 
 from actions import rasa_callbacks
 
 
 @pytest.mark.asyncio
 async def test_ask_to_join(
-        patch_rasa_callbacks: Callable[[Text, Text, Dict[Text, Any]], AsyncMock],
+        rasa_callbacks_expected_call_builder: Callable[[Text, Text, Dict[Text, Any]], Tuple[Text, call]],
+        mock_aioresponses: aioresponses,
         external_intent_response: Dict[Text, Any],
 ) -> None:
-    mock_rasa_callbacks = patch_rasa_callbacks(
+    expected_rasa_url, expected_rasa_call = rasa_callbacks_expected_call_builder(
         'partner_id_to_ask',
         'EXTERNAL_ask_to_join',
         {
             'partner_id': 'id_of_asker',
         },
     )
+    mock_rasa_callbacks = AsyncMock(return_value=CallbackResult(payload=external_intent_response))
+    mock_aioresponses.post(
+        expected_rasa_url,
+        callback=mock_rasa_callbacks,
+    )
 
     assert await rasa_callbacks.ask_to_join('id_of_asker', 'partner_id_to_ask') == external_intent_response
-    mock_rasa_callbacks.assert_called_once()  # more detailed assertions are done by the fixture
+    assert mock_rasa_callbacks.mock_calls == [expected_rasa_call]
 
 
 @pytest.mark.asyncio
