90b62a830a64ad7054f8c9966e38e3a12a5167eb
Oleksandr Tereshchenko <toporok@gmail.com>
2021-05-27 19:54:37 +0300

    in progress: test_actions - mock things on on a deeper abstraction level for tests to be more effective

--- tests/actions/test_rasa_callbacks.py
+++ tests/actions/test_rasa_callbacks.py
@@ -26,16 +26,21 @@ async def test_ask_to_join(
         callback=mock_rasa_callbacks,
     )
 
-    assert await rasa_callbacks.ask_to_join('id_of_asker', 'partner_id_to_ask') == external_intent_response
+    assert await rasa_callbacks.ask_to_join(
+        'id_of_asker',
+        'partner_id_to_ask',
+    ) == external_intent_response
+
     assert mock_rasa_callbacks.mock_calls == [expected_rasa_call]
 
 
 @pytest.mark.asyncio
 async def test_join_room(
-        patch_rasa_callbacks: Callable[[Text, Text, Dict[Text, Any]], AsyncMock],
+        rasa_callbacks_expected_call_builder: Callable[[Text, Text, Dict[Text, Any]], Tuple[Text, call]],
+        mock_aioresponses: aioresponses,
         external_intent_response: Dict[Text, Any],
 ) -> None:
-    mock_rasa_callbacks = patch_rasa_callbacks(
+    expected_rasa_url, expected_rasa_call = rasa_callbacks_expected_call_builder(
         'a_receiving_user',
         'EXTERNAL_join_room',
         {
@@ -43,25 +48,41 @@ async def test_join_room(
             'room_url': 'https://room-unittest/url',
         },
     )
+    mock_rasa_callbacks = AsyncMock(return_value=CallbackResult(payload=external_intent_response))
+    mock_aioresponses.post(
+        expected_rasa_url,
+        callback=mock_rasa_callbacks,
+    )
 
     assert await rasa_callbacks.join_room(
         'a_sending_user',
         'a_receiving_user',
         'https://room-unittest/url',
     ) == external_intent_response
-    mock_rasa_callbacks.assert_called_once()  # more detailed assertions are done by the fixture
+
+    assert mock_rasa_callbacks.mock_calls == [expected_rasa_call]
 
 
 @pytest.mark.asyncio
 async def test_find_partner(
-        patch_rasa_callbacks: Callable[[Text, Text, Dict[Text, Any]], AsyncMock],
+        rasa_callbacks_expected_call_builder: Callable[[Text, Text, Dict[Text, Any]], Tuple[Text, call]],
+        mock_aioresponses: aioresponses,
         external_intent_response: Dict[Text, Any],
 ) -> None:
-    mock_rasa_callbacks = patch_rasa_callbacks(
+    expected_rasa_url, expected_rasa_call = rasa_callbacks_expected_call_builder(
         'a_receiving_user',
         'EXTERNAL_find_partner',
         {},
     )
+    mock_rasa_callbacks = AsyncMock(return_value=CallbackResult(payload=external_intent_response))
+    mock_aioresponses.post(
+        expected_rasa_url,
+        callback=mock_rasa_callbacks,
+    )
 
-    assert await rasa_callbacks.find_partner('some_sender_id', 'a_receiving_user') == external_intent_response
-    mock_rasa_callbacks.assert_called_once()  # more detailed assertions are done by the fixture
+    assert await rasa_callbacks.find_partner(
+        'some_sender_id',
+        'a_receiving_user',
+    ) == external_intent_response
+
+    assert mock_rasa_callbacks.mock_calls == [expected_rasa_call]
