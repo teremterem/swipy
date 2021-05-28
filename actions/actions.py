40489545651062e4bc83499d4afb7563deb30a77
Oleksandr Tereshchenko <toporok@gmail.com>
2021-05-28 15:28:39 +0300

    in progress: more logging to investigate telegram message limit problem

--- actions/actions.py
+++ actions/actions.py
@@ -56,7 +56,7 @@ class BaseSwiperAction(Action, ABC):
             tracker: Tracker,
             domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-        logger.info('BEGIN ACTION RUN: %s', self.name())  # TODO oleksandr: debug instead of info ?
+        logger.info('BEGIN ACTION RUN: %s (tracker.sender_id=%r)', self.name(), tracker.sender_id)
 
         user_vault = UserVault()
 
@@ -110,7 +110,7 @@ class BaseSwiperAction(Action, ABC):
             ),
         ])
 
-        logger.info('END ACTION RUN: %s', self.name())  # TODO oleksandr: debug instead of info ?
+        logger.info('END ACTION RUN: %s (tracker.sender_id=%r)', self.name(), tracker.sender_id)
         return events
 
 
