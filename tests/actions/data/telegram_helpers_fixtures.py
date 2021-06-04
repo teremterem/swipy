9bba9398ecac130cf4d72d35ba19af2835c6635f
Oleksandr Tereshchenko <toporok@gmail.com>
2021-06-04 20:21:27 +0300

    in progress: telebot.get_user_profile_photos

--- tests/actions/data/telegram_helpers_fixtures.py
+++ tests/actions/data/telegram_helpers_fixtures.py
@@ -22,23 +22,23 @@ def telegram_user_profile_photo() -> Dict[Text, Any]:
         'photos': [
             [
                 {
-                    'file_id': 'smallest_file_id',
+                    'file_id': 'smallest_profile_pic_file_id',
                     'file_size': 10450,
-                    'file_unique_id': 'smallest_file_unique_id',
+                    'file_unique_id': 'smallest_profile_pic_file_unique_id',
                     'height': 160,
                     'width': 160,
                 },
                 {
-                    'file_id': 'biggest_file_id',
+                    'file_id': 'biggest_profile_pic_file_id',
                     'file_size': 102191,
-                    'file_unique_id': 'biggest_file_unique_id',
+                    'file_unique_id': 'biggest_profile_pic_file_unique_id',
                     'height': 640,
                     'width': 640,
                 },
                 {
-                    'file_id': 'medium_file_id',
+                    'file_id': 'medium_profile_pic_file_id',
                     'file_size': 33842,
-                    'file_unique_id': 'medium_file_unique_id',
+                    'file_unique_id': 'medium_profile_pic_file_unique_id',
                     'height': 320,
                     'width': 320,
                 },
