3f6b5af126803e78d3d4410d9d3e8f32e478ebfc
Oleksandr Tereshchenko <toporok@gmail.com>
2021-06-07 19:39:40 +0300

    greeting automatically makes user ok to chitchat

--- actions/actions.py
+++ actions/actions.py
@@ -186,7 +186,11 @@ class ActionRegisterUser(BaseSwiperAction):
             user_vault: IUserVault,
     ) -> List[Dict[Text, Any]]:
         if NEW_USERS_ARE_OK_TO_CHITCHAT:
-            if current_user.state == UserState.NEW:
+            if current_user.state in (
+                    UserState.NEW,
+                    UserState.DO_NOT_DISTURB,
+                    UserState.ASKED_TO_JOIN,
+            ):
                 # noinspection PyUnresolvedReferences
                 current_user.become_ok_to_chitchat()
                 user_vault.save(current_user)
