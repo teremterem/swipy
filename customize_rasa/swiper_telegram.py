0f7c2c7feec6b326cdb5655ce2c4a87fa782cb45
Oleksandr Tereshchenko <toporok@gmail.com>
2021-06-12 15:44:29 +0300

    in progress: SwiperTelegramInput

--- customize_rasa/swiper_telegram.py
+++ customize_rasa/swiper_telegram.py
@@ -3,23 +3,19 @@ from typing import Callable, Awaitable, Any
 from rasa.core.channels import TelegramInput, UserMessage
 from sanic import Blueprint
 
+START_DEEPLINK_PREFIX = '/start '
+
 
 class SwiperTelegramInput(TelegramInput):
     def blueprint(
             self, on_new_message: Callable[[UserMessage], Awaitable[Any]]
     ) -> Blueprint:
         async def handler(message: UserMessage) -> Any:
-            print()
-            print()
-            print()
-            print('SWIPER - BEFORE HANDLER')
-            print()
+            if message.text and message.text.startswith(START_DEEPLINK_PREFIX):
+                deeplink_data = message.text[len(START_DEEPLINK_PREFIX):]
+                message.metadata = message.metadata or {}
+                message.metadata['deeplink_data'] = deeplink_data
             res = await on_new_message(message)
-            print()
-            print('SWIPER - AFTER HANDLER')
-            print()
-            print()
-            print()
             return res
 
         return super().blueprint(handler)
