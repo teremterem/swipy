d677dea8a7d33b7833de89ec1cb237572278c2ea
Oleksandr Tereshchenko <toporok@gmail.com>
2021-07-03 16:48:27 +0300

    in progress: mark partner as bot_blocked if rasa callback fails with Telegram 403

--- tests/actions/test_rasa_callbacks.py
+++ tests/actions/test_rasa_callbacks.py
@@ -1,5 +1,5 @@
 import re
-from typing import Dict, Text, Any, Callable, Tuple
+from typing import Dict, Text, Any, Callable, Tuple, Awaitable
 from unittest.mock import patch, AsyncMock
 
 import pytest
@@ -13,14 +13,20 @@ from actions.utils import SwiperRasaCallbackError
 
 
 @pytest.mark.asyncio
+@pytest.mark.parametrize('function_to_test, expected_intent', [
+    (rasa_callbacks.ask_to_join, 'EXTERNAL_ask_to_join'),
+    (rasa_callbacks.ask_to_confirm, 'EXTERNAL_ask_to_confirm'),
+])
 @patch('actions.rasa_callbacks._trigger_external_rasa_intent', wraps=rasa_callbacks._trigger_external_rasa_intent)
-async def test_ask_to_join(
+async def test_ask_to_join_and_confirm(
         wrap_trigger_external_rasa_intent: AsyncMock,
         rasa_callbacks_expected_req_builder: Callable[
             [Text, Text, Dict[Text, Any]], Tuple[Tuple[Text, URL], RequestCall]
         ],
         mock_aioresponses: aioresponses,
         external_intent_response: Dict[Text, Any],
+        function_to_test: Callable[[Text, UserStateMachine, Text], Awaitable[Dict[Text, Any]]],
+        expected_intent: Text,
 ) -> None:
     expected_req_key, expected_req_call = rasa_callbacks_expected_req_builder(
         'partner_id_to_ask',
@@ -32,7 +38,7 @@ async def test_ask_to_join(
     )
     mock_aioresponses.post(re.compile(r'.*'), payload=external_intent_response)
 
-    assert await rasa_callbacks.ask_to_join(
+    assert await function_to_test(
         'id_of_asker',
         UserStateMachine(user_id='partner_id_to_ask'),
         'photo_of_the_asker',
