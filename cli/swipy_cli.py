d0c1e7ee181dee7c658b92dd99b8bfb62e474c1b
Oleksandr Tereshchenko <toporok@gmail.com>
2021-07-07 20:10:04 +0300

    cli: make everyone do_not_disturb

--- cli/swipy_cli.py
+++ cli/swipy_cli.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from typing import Text
 
 import boto3
 import click
@@ -14,21 +15,26 @@ AWS_REGION = os.environ['AWS_REGION']
 dynamodb = boto3.resource('dynamodb', AWS_REGION)
 
 
-@click.command()
-def make_all_do_not_disturb():
+def _set_everyones_state(state: Text) -> None:
     user_state_machine_table_name = prompt('Please enter the name of UserStateMachine DDB table')
     user_state_machine_table = dynamodb.Table(user_state_machine_table_name)
 
     counter = 0
-    with user_state_machine_table.batch_writer() as writer:
-        for item in user_state_machine_table.scan()['Items']:
-            writer.update_item({
-                'user_id': item['user_id'],
-                'state': UserState.DO_NOT_DISTURB,
-            })
-            counter += 1
+    for item in user_state_machine_table.scan()['Items']:
+        user_state_machine_table.update_item(
+            Key={'user_id': item['user_id']},
+            UpdateExpression='set #state=:state',
+            ExpressionAttributeNames={'#state': 'state'},
+            ExpressionAttributeValues={':state': state},
+        )
+        counter += 1
     print('DONE FOR', counter, 'ITEMS')
 
 
+@click.command()
+def make_everyone_do_not_disturb() -> None:
+    _set_everyones_state(UserState.DO_NOT_DISTURB)
+
+
 if __name__ == '__main__':
-    make_all_do_not_disturb()
+    make_everyone_do_not_disturb()
